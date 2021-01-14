cc72e576d4b22df89746423036e84db67ddeebdf
Param Attri <attri.param@gmail.com>
2021-01-14 17:06:24 +0530

    payment with stripe

--- config-based-hooks-form-v2/src/components/RenderQuote.js
+++ config-based-hooks-form-v2/src/components/RenderQuote.js
@@ -1,19 +1,38 @@
-import { Grid } from '@material-ui/core'
+import { Button, Grid } from '@material-ui/core'
 import React from 'react'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 function RenderQuote({locationData}) {
 
     const quoteData = JSON.parse(locationData.location.state.quoteData)
     console.log(quoteData)
 
+    const handleToken = (token, addresses) => {
+        //Have to send the product also to the backend {token, product}
+        axios.post("", {token})
+    }
+
+    const stripePayment = (partner, premium) => {
+        return <StripeCheckout
+            stripeKey=""
+            token={handleToken}
+            amount={premium}
+            name={partner}
+            billingAddress
+            shippingAddress
+        />
+    }
+
     const capitalize = (str)=>{
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
+    
     const renderQuote = (quote) => {
         let quotes=[]
         for(let key in quote){
@@ -41,6 +60,7 @@ function RenderQuote({locationData}) {
                         {/* {(typeof (partner.quote) === 'object') && <>{renderQuote(partner.quote)}</>} */}
                         {renderQuote(partner)}
                     </CardContent>
+                    <Button onClick={() => stripePayment(partner.partner, partner.premium)}>Buy Now</Button>
                 </Card>
             </Grid>
         )
